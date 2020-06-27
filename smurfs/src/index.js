a5d954e7e0fa78ef0eda044c4098d5dce9614c11
RyanMersmann <mersmannryan@gmail.com>
2020-06-27 10:28:48 -0600

    added dependencies and reducer/actions files

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -7,8 +7,6 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { smurfReducer } from './reducers';
+// import { reducer } from './reducers';
 
-const store = createStore(smurfReducer, applyMiddleware(thunk, logger));
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
